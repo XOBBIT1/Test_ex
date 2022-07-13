259b7a78a81e10f10b26196db4bb6d5395ff96ff
Xobbit <ben300300@gmail.com>
2022-07-13 18:36:57 +0300

    all

--- mysite/abbreviations/views.py
+++ mysite/abbreviations/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from . import service
 from django.urls import reverse
 
 def index(request):
